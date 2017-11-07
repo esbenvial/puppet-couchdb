7f5b942e7a91d1b0bcd5fedd834d5f33e55b211e
Esben Vial <cjs614@hum.ku.dk>
2017-11-07 17:32:52 +0100

    upgrade couchdb

--- examples/couchdb.pp
+++ examples/couchdb.pp
@@ -1,1 +1,5 @@
-class { 'couchdb': }
+class { 'couchdb':
+  bind_address   => '0.0.0.0',
+  admin_password => 'asdf1234',
+  http_secret    => '8bb8f92c2ea0431d6d974473bb4a11d1',
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,11 @@
 class couchdb (
   $admin_name                 = 'admin',
   $admin_password             = 'admin',
+  $http_secret                = '8bb8f92c2ea0431d6d974473bb4a11d1',
   $allow_jsonp                = false,
   $authentication_handlers    = '{couch_httpd_oauth, oauth_authentication_handler}, {couch_httpd_auth, cookie_authentication_handler}, {couch_httpd_auth, default_authentication_handler}',
   $bind_address               = '127.0.0.1',
-  $couchdb_conf_dir           = '/etc/couchdb',
+  $couchdb_conf_dir           = '/opt/couchdb/etc',
   $database_dir               = '/var/lib/couchdb',
   $delayed_commits            = true,
   $default_handler            = '{couch_httpd_db, handle_request}',
@@ -32,43 +33,80 @@ class couchdb (
 
   include ::apt
 
-  apt::ppa { 'ppa:couchdb/stable': }
+  apt::source { 'couchdb':
+    location => 'https://apache.bintray.com/couchdb-deb',
+    repos    => 'main',
+    #pin      => '-10',
+    key      => {
+      'id'     => '15866BAFD9BCC4F3C1E0DFC7D69548E1C17EAB57',
+      'source' => 'https://couchdb.apache.org/repo/bintray-pubkey.asc',
+    },
+    include  => {
+      'deb' => true,
+    },
+  }
 
   package { 'couchdb':
     ensure => present,
-    require => Apt::Ppa['ppa:couchdb/stable'],
+    require => Apt::Source['couchdb'],
+  }
+
+  File {
+    owner => 'couchdb',
+    group => 'couchdb',
+    mode  => '0640',
   }
 
   file { 'uuid.ini':
     ensure => present,
-    path   => "${couchdb_conf_dir}/local.d/uuid.ini"
+    path   => "${couchdb_conf_dir}/local.d/uuid.ini",
+    require => Package['couchdb'],
   }
 
-  $local_ini = "${couchdb_conf_dir}/local.ini"
+  $default_dir = "${couchdb_conf_dir}/default.d"
+  $local_dir   = "${couchdb_conf_dir}/local.d"
 
   Ini_Setting {
     ensure  => present,
-    path    => $local_ini,
+    notify  => Service['couchdb'],
+    require => Package['couchdb'],
   }
 
-  ini_setting { 'couchdb-ini-bind_address':
-    section => 'httpd',
+  ini_setting { 'couchdb-${default_dir}-chttpd-bind_address':
+    path    => "${default_dir}/10-bind-address.ini",
+    section => 'chttpd',
     setting => 'bind_address',
     value   => $bind_address,
   }
 
-  ini_setting { 'couchdb-ini-bind_address':
-    ensure  => present,
-    path    => $local_ini,
+  ini_setting { 'couchdb-${local_dir}-admins-admin':
+    path    => "${local_dir}/10-admins.ini",
     section => 'admins',
     setting => $admin_name,
     value   => $admin_password,
   }
-  ini_setting { 'couchdb-ini-ssl_certificate_max_depth':
-    ensure  => present,
-    path    => $local_ini,
-    section => 'ssl',
-    setting => 'ssl_certificate_max_depth',
-    value   => 1,
+
+  ini_setting { 'couchdb-${local_dir}-couch_httpd_auth-secret':
+    path    => "${local_dir}/10-admins.ini",
+    section => 'couch_httpd_auth',
+    setting => 'secret',
+    value   => $http_secret,
+  }
+
+  file { "${default_dir}/10-bind-address.ini":
+    require => [
+      Ini_setting['couchdb-${default_dir}-chttpd-bind_address'],
+    ],
+  }
+  file { "${local_dir}/10-admins.ini":
+    require => [
+      Ini_setting['couchdb-${local_dir}-couch_httpd_auth-secret'],
+      Ini_setting['couchdb-${local_dir}-admins-admin'],
+    ],
+  }
+
+  service { 'couchdb':
+    ensure => running,
+    enable => true,
   }
 }
